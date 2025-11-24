bbc0a9de74ede9be3fcc809ba39daf25396fa472
bogachenkove <8535285+bogachenkove@users.noreply.github.com>
2025-11-24 23:37:34 +0300

    Modify Raspberry Pi setup script for packages and services
    Updated package installations and firewall rules. Added service management for enabling and disabling specific services.

--- scripts/raspberrypi-aarch64.sh
+++ scripts/raspberrypi-aarch64.sh
@@ -24,7 +24,7 @@ fi
 curl -o ~/00-apt-conf https://raw.githubusercontent.com/bogachenko/lib/master/config/raspberrypi-aarch64/00-apt-conf;sudo mv ~/00-apt-conf /etc/apt/apt.conf.d/00-apt-conf
 
 # Configuring localization files
-sudo sh -c "echo \"en_US.UTF-8 UTF-8\" > /etc/locale.gen";sudo locale-gen >> /dev/null;sudo sh -c "echo \"en_US.UTF-8 UTF-8\" > /etc/default/locale"
+sudo sh -c "echo \"en_US.UTF-8 UTF-8\" > /etc/locale.gen";sudo locale-gen >> /dev/null;sudo sh -c "echo \"en_US.UTF-8 UTF-8\" > /etc/default/locale";echo 'LANG=en_US.UTF-8' | sudo tee -a /etc/environment;echo 'LC_ALL=en_US.UTF-8' | sudo tee -a /etc/environment
 
 echo 'Updating the package list.'
 sudo apt update && sudo apt upgrade
@@ -32,15 +32,16 @@ sudo apt update && sudo apt upgrade
 echo 'Installing the core packages.'
 sudo apt install --no-install-recommends --no-install-suggests --yes \
  xorg mesa-utils lm-sensors htop ufw gpm apache2 cmake plymouth vim git lshw dnsmasq hostapd \
- encfs cryfs whois default-jre-headless
+ encfs cryfs whois default-jre-headless lsof
 echo 'Installing the sub-core packages.'
 sudo apt install --no-install-recommends --no-install-suggests --yes \
  wireplumber pipewire pipewire-jack pipewire-alsa pipewire-pulse ffmpeg i2pd tor obfs4proxy privoxy \
  fonts-ubuntu fonts-noto-color-emoji fonts-noto-mono fonts-noto fonts-liberation fonts-dejavu \
- fonts-noto-cjk ttf-mscorefonts-installer fonts-font-awesome ranger
+ fonts-noto-cjk ttf-mscorefonts-installer fonts-font-awesome ranger realvnc-vnc-server realvnc-vnc-viewer
 echo 'Installing the extra packages.'
 sudo apt install --no-install-recommends --no-install-suggests --yes \
- tmux firefox chromium vlc scrot rxvt-unicode speedtest-cli retroarch yt-dlp transmission-cli \
+ tmux firefox chromium vlc scrot rxvt-unicode speedtest-cli retroarch yt-dlp transmission-cli sddm \
+ awesome feh
 curl -fsSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
 curl -fsSL https://raw.githubusercontent.com/AdguardTeam/AdGuardVPNCLI/HEAD/scripts/release/install.sh | sh -s -- -v
 curl -fsSL https://raw.githubusercontent.com/AdguardTeam/AdGuardCLI/release/install.sh | sh -s -- -v
@@ -59,9 +60,28 @@ sudo ufw allow 68/udp           # DHCP client v4
 sudo ufw allow 546/udp          # DHCP client v6
 sudo ufw allow 547/udp          # DHCP server v6
 sudo ufw allow 80               # HTTP
-sudo ufw allow 8082             # Apache
+sudo ufw allow 8080             # Apache
+sudo ufw allow 8081             # AdGuardHome control panel
+#sudo ufw allow 3129             # AdGuard HTTP
+#sudo ufw allow 1081             # AdGuard SOCKS5
 sudo ufw allow 8118/tcp         # Privoxy
 sudo ufw allow 853              # DoT/DoQ
 sudo ufw allow 9050/tcp         # TOR
 sudo ufw allow 993              # IMAPS
 sudo ufw enable
+
+echo 'Enabling services.'
+enable_services=(
+ vncserver-virtuald
+ vncserver-x11-serviced
+)
+disable_services=(
+  dnsmasq
+  hostapd
+)
+for service in "${enable_services[@]}"; do
+  sudo systemctl enable "${service}.service"
+done
+for service in "${disable_services[@]}"; do
+  sudo systemctl disable "${service}.service"
+done
